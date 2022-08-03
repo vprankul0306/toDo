d23a38a253affb27a7a00551011fe04716916dab
Prankul verma <62751285+vprankul0306@users.noreply.github.com>
2022-08-03 17:47:33 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.listen(3000, function () {
+  console.log("server started!!!!");
+});
